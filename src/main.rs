972d3a0f6e0d5d9d9abb780179e814c5e8b98aa3
Jubi Taneja <jubi@cs.utah.edu>
2018-06-21 14:07:17 -0700

    major clanup

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::str::CharIndices;
 
+// Types of Tokens
 pub enum TokKind<'a>{
-//pub enum TokKind {
     Error,
     Ident,
     ValName,
@@ -19,35 +19,32 @@ pub enum TokKind<'a>{
     Eof,
 }
 
-//pub struct Token {
-//    //kind: Kind,
-//    name: &str,
-//    width: u32,
-//}
-
+// Error type
 pub enum Error {
     InvalidChar,
 }
 
-/// An `Error` with an associated Location.
+// Error with location and error string
 pub struct LocatedError {
     pub error: Error,
     pub errmsg: String,
     pub location: Location,
 }
 
+// Token with type and location
 pub struct LocatedToken<'a> {
-//pub struct LocatedToken {
     pub kind: TokKind<'a>,
     pub location: Location,
 }
 
+// Line number specifies the location
+// of token or error
 #[derive(Clone)]
 pub struct Location {     
     pub line_num: usize,
 }
 
-//jubi: ret type result<locatedtoken<'a>>
+// Build the token with all attributes
 fn token<'a>(token: TokKind<'a>, loc: Location) -> Result<LocatedToken<'a>, LocatedError> {
     Ok(LocatedToken {
         kind: token,
@@ -55,6 +52,7 @@ fn token<'a>(token: TokKind<'a>, loc: Location) -> Result<LocatedToken<'a>, Loca
     })
 }
 
+// Build the error with all attributes
 fn error(err: Error, msg: String, loc: Location) -> Result<(), LocatedError> {
     Err(LocatedError {
         error: err,
@@ -63,6 +61,7 @@ fn error(err: Error, msg: String, loc: Location) -> Result<(), LocatedError> {
     })
 }
 
+// Lexer
 #[derive(Clone)]
 pub struct Lexer<'a> {
     source: &'a str,
@@ -81,6 +80,7 @@ pub struct Lexer<'a> {
 }
 
 impl<'a> Lexer<'a> {
+    // Initialize the lexer.
     pub fn new(s: &str) -> Lexer {
         let mut lex = Lexer {
             source: s,
@@ -89,25 +89,20 @@ impl<'a> Lexer<'a> {
             pos: 0,
             line_number: 1,
         };
-        //get to the first char of source text
-        println!("****** Call next_ch() from new() ******");
         lex.next_ch();
         lex
     }
 
+    // Is the currnt character alphabet(a-z | A-Z)) or digit (0-9)?
     pub fn is_alphadigit(&mut self, ch: Option<char>) -> bool {
-        println!("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t in while starting current ch == {:?}", ch);
         match ch {
             Some('0' ... '9') => {
-                println!("ch in 0-9 is = {:?}", self.lookahead);
                 true
             },
             Some('a' ... 'z') => {
-                println!("ch in a-z is = {:?}", self.lookahead);
                 true
             },
             Some('A' ... 'Z') => {
-                println!("ch in A-Z is = {:?}", self.lookahead);
                 true
             },
             Some(_) => {
@@ -119,33 +114,27 @@ impl<'a> Lexer<'a> {
          }
     }
 
+    // Get the next character.
     fn next_ch(&mut self) -> Option<char> {
         // TODO: do we need to look for '\n' at the first?
         // If so, then handle it and increment line_number
-        println!("ch in next_ch starting is:::::: {:?}", self.lookahead);
          if self.lookahead == Some('\n') {
-             println!("Am I newline char??????????");
              self.line_number += 1;
         }
         match self.chars.next() {
-            // look for all posible chars in souper IR
             Some((idx, ch)) => {
                 self.lookahead = Some(ch);
                 self.pos = idx;
-                println!("No specific match of particular char here --- ");
-                println!("pos of ch === {}", self.pos);
-                println!("first match in ch ==== {:?}", self.lookahead);
             },
             None => {
-                println!("++++++++++++ In None match in next_ch() func ++++++");
                 self.pos = self.source.len();
-                println!("Pos on eof None matcher === {}", self.pos);
                 self.lookahead = None;
             }
         }
         self.lookahead
     }
 
+    // Scan rest of the commented line starting with ';'.
     pub fn rest_of_line(&mut self) -> &'a str {
         let begin = self.pos;
         loop {
@@ -157,19 +146,15 @@ impl<'a> Lexer<'a> {
     }
 
     // Scan a comment extending to the end of the current line.
-    // //jubi: ret type result<locatedtoken<'a>>
     fn scan_comment(&mut self) -> Result<LocatedToken<'a>, LocatedError> {
         let loc = self.loc();
         let text = self.rest_of_line();
         token(TokKind::Comment(text), loc)
     }
 
-    // scan all chars except newline, comments, and
-    // return a valid token type
-    // //jubi: ret type result<locatedtoken<'a>>
+    // Scan all useful characters here (except, newline, comments, etc.).
     fn scan_rest(&mut self) -> Result<LocatedToken<'a>, LocatedError> {
         let loc = self.loc();
-        println!("specific match begins now for % - , - = - [a-z] ...");
         match self.lookahead {
             // FIXME: ideally there won't be None here, because
             // its handled by getNextToken()
@@ -183,90 +168,52 @@ impl<'a> Lexer<'a> {
                 token(TokKind::Equal, loc)
             },
             Some('%') => {
-                println!("%%%%%%%%%%%%%");
-                println!("ch == {:?}", self.lookahead);
-                println!("call to get next_ch now");
                 self.next_ch();
-                println!("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-                println!("Lookahead char is == {:?}", self.lookahead);
-                println!("\t\t\t\t\t\t\t\tpos begin == {}", self.pos);
-                println!("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
                 let mut startPos = self.pos;
-                // TODO: refactor this loop (make a func: self.is_alpha_digit(sef.lookahead))
                 let mut current_ch = self.lookahead.clone();
-                println!("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t current ch == {:?}", current_ch);
+
                 while self.is_alphadigit(current_ch) {
                     self.next_ch();
                     current_ch = self.lookahead.clone();
                 }
-//                loop {
-//                    match self.lookahead {
-//                        Some('0' ... '9') => {
-//                            println!("ch in 0-9 is = {:?}", self.lookahead);
-//                            self.next_ch();
-//                            continue;
-//                        },
-//                        Some('a' ... 'z') => {
-//                            println!("ch in a-z is = {:?}", self.lookahead);
-//                            self.next_ch();
-//                            continue;
-//                        },
-//                        Some('A' ... 'Z') => {
-//                            println!("ch in A-Z is = {:?}", self.lookahead);
-//                            self.next_ch();
-//                            continue;
-//                        },
-//                        Some(_) => {
-//                            break;
-//                        },
-//                        _ => {
-//                            break;
-//                        },
-//                    };
-//                }
-                println!("\t\t\t\t\t\t\t\tpos end == {}", self.pos);
-//                let errloc = loc;
+
+                //FIXME: we want to exit here if error occurs
                 if self.pos - startPos == 0 {
-                    //FIXME: we want to exit here if error occurs
                     error(Error::InvalidChar, "expected an identifier".to_string(), loc.clone());
                 }
-                //while (self.is_alphadigit(self.lookahead)) {
-                  //  self.next_ch();
-                //}
+
                 token(TokKind::Ident, loc)
             },
             _ => {
-                println!("Faltu ka case -- hata do isko");
-                println!("ch == {:?}", self.lookahead);
+                // FIXME: I think this is not required, do something else
+                // with this case.
                 self.next_ch();
-                println!("returned from next_ch __________");
+                // FIXME: random token type was added for the time being
                 token(TokKind::ValName, loc)
             }
         }
     }
 
+    // Build the location for tokens or errors
     fn loc(&self) -> Location {
         Location {
             line_num: self.line_number,
         }
     }
 
+    // Get next token. This function is a driver to invoke the token generator
+    // (scan_rest) to scan the meaningful characters.
     pub fn getNextToken(&mut self) -> Option<Result<LocatedToken<'a>, LocatedError>> {
-        println!("------------- In getnexttoken() ----------------");
         loop {
-            println!("----------------------- Inner loop in next -------------------------");
             let loc = self.loc();
-            println!("Line number === {}", loc.line_num);
-            println!("__________ Lookahead after everything is :::::::::: {:?}", self.lookahead);
             match self.lookahead {
                 None => {
-                    println!("---------- eof is reached ---");
+                    // Break with an EOF token.
                     break Some(token(TokKind::Eof, loc));
-                    //break Some(error(Error::InvalidChar, loc));
                 },
                 Some('\n') => {
-                    println!("********************************what??? *****");
                     self.next_ch();
+                    // FIXME: Do we need to do it here as well?
                     //self.line_number += 1;
                     continue;
                 },
@@ -292,9 +239,7 @@ impl<'a> Lexer<'a> {
                     continue;
                 },
                 _ => {
-                    println!("in specific case matching now, but nothing for % yet");
                     Some(self.scan_rest());
-                    println!("returned call from scan_rest _______");
                     continue;
                 },
             }
@@ -302,14 +247,14 @@ impl<'a> Lexer<'a> {
     }
 }
 
+// Lexer Driver
 fn startLexer(text: & str) {
     let mut inputLex = Lexer::new(text);
+    // Lex until EOF token is found.
     loop {
-        println!("---- Major loop begins ------");
         let tok = inputLex.getNextToken();
         match tok {
             Some(Ok(LocatedToken { kind, location })) => {
-                println!("In the finale");
                 break;
             },
             Some(_) => {},
@@ -331,8 +276,6 @@ fn main () {
     let mut contents = String::new();
     file.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
-
-    println!("With text:{}", contents);
 
     startLexer(&contents);
 }
